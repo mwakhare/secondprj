f0b7cfc68f93b878ae05f7daec342d37d7fb4241
Milind Wakhare <Milind Wakhare>
2017-01-27 18:08:28 +0530

    db.js created

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require ('express');
+//var bodyparser = require ('body-parser');
+var session = require ('express-session');
+
+//var db = require ('./models/db.js');  // db.js must be required before routes.js
+var app = express(); // exporting apps must be done before routes.js
+//var routes = require ('./routes/routes.js');
+
+
+//app.use (express.static (__dirname + "/public"));
+//app.use (bodyparser.json ());
+
+//app.use (bodyparser.urlencoded ({extended:false}));
+//app.use (session ({secret: "secret",  resave : true,  saveUninitialized : false}));
+
+//app.post ('/auth', route.authHandler);
+//app.post ('/register', route.registerUserHandler);
+
+// REST Routes
+//app.get ('/v1/feedbacks', route.getAllHandler);  // return all feedbacks records
+//app.get ('/v1/feedbacks/:feedback', route.getOneHandler);  // return one record
+//app.post ('/v1/feedbacks', route.postOneHandler); // add new tech record
+//app.put ('/v1/feedbacks/:feedback', route.updateOneHandler); // update a record
+//app.delete ('/v1/feedbacks/:feedback', route.deleteOneHandler); // detete a record
+
+
+var port = process.env.PORT || 3000;
+
+app.listen (port, function ()
+{
+	console.log ('HTTP server is listening on port: ' + port);
+});
---
+++ models/db.js
@@ -0,0 +1,52 @@
+//var chalk = require('chalk');
+var mongoose = require( 'mongoose' );
+
+
+var dbURI = 'mongodb://127.0.0.1/restfulDB';
+//var dbURI =  'mongodb://edu:edu@ds015879.mlab.com:15879/edurekadb';
+
+console.log ("Establishing connection to the DB");
+
+//   ****** CONNECTIONS
+mongoose.connect (dbURI);
+
+mongoose.connection.on ('connected', function () 
+{
+  console.log ('Mongoose connected to ' + dbURI);
+}
+);
+
+mongoose.connection.on ('error', function (err) 
+{
+  console.log ('Mongoose connection error: ' + err);
+}
+);
+
+mongoose.connection.on ('disconnected', function () 
+{
+  console.log ('Mongoose disconnected');
+}
+);
+
+// ***** *******  *  *****   Schema defs
+// var userSchema = new mongoose.Schema({
+//   username: {type: String, unique:true},
+//   location: {type: String},
+//   password: String
+// }, {collection: 'UsersCol'});
+
+// var feedBackSchema = new mongoose.Schema({
+//   name: {type: String},
+//   location: {type: String},
+//   response1: {type: String},
+//   response2: {type: String},
+//   response3: {type: String},
+//   response4: {type: String},
+//   comment: {type: String}
+
+// }, {collection: 'FeedCol'});
+
+// // register the User model
+// mongoose.model( 'UserModel', userSchema);
+// mongoose.model( 'FeedBackModel', feedBackSchema);
+
